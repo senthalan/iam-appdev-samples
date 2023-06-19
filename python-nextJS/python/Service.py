1364ac8b876ce25e59a0002aa4cd98461b698f54
senthalan <senthalank@gmail.com>
2023-06-19 16:21:06 +0530

    Add initial impl

--- python-nextJS/python/Service.py
+++ python-nextJS/python/Service.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+
 import os
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-  return 'Server Works!'
+items = []  # In-memory list to store items
+
+# GET API to retrieve all items
+@app.route('/items', methods=['GET'])
+def get_items():
+    return jsonify(items)
+
+# POST API to add an item
+@app.route('/items', methods=['POST'])
+def add_item():
+    item = request.json
+    items.append(item)
+    return jsonify({'message': 'Item added successfully'})
 
 
 if __name__ == "__main__":
