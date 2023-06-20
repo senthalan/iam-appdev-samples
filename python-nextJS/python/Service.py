6ac1982a80547dad6e1c83ce5564f8ed99397aec
senthalan <senthalank@gmail.com>
2023-06-20 17:07:32 +0530

    Log user details in the service

--- python-nextJS/python/Service.py
+++ python-nextJS/python/Service.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify, request
-
+import jwt
 import os
 
 app = Flask(__name__)
@@ -10,6 +10,15 @@ items = []  # In-memory list to store items
 # GET API to retrieve all items
 @app.route('/items', methods=['GET'])
 def get_items():
+
+    if request.headers.get('x-jwt-assertion'):
+        token = request.headers['x-jwt-assertion']
+    else:
+        return jsonify({'message': 'Token not found'}), 400
+    token_info = decode_access_token(token)
+    print('token_info : ', token_info)
+    username = token_info.get('email')
+    print('username : ', username)
     return jsonify(items)
 
 # POST API to add an item
@@ -20,6 +29,26 @@ def add_item():
     return jsonify({'message': 'Item added successfully'})
 
 
+def decode_access_token(token):
+
+    # # get public key from jwks uri
+    # response = httpx.get(url="your open id wellknown url to fetch public key")
+
+    # # gives the set of jwks keys.the keys has to be passed as it is to jwt.decode() for signature verification.
+    # key = response.json()
+
+    # # get the algorithm type from the request header
+    # algorithm = jwt.get_unverified_header(token).get('alg')
+
+    # user_info = jwt.decode(token,
+    #                        key=key,
+    #                        algorithms=algorithm)
+    user_info = jwt.decode(token, options={"verify_signature": False})
+
+    return user_info
+
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=False, host='0.0.0.0', port=port)
+
